698499b87c2bc5193b6101b07bc11597f5c55a9b
Michael B. Syson <masarapmabuhay@gmail.com>
2018-10-30 20:29:36 +0800

    +updated: Java instructions to auto-calculate and generate Doctor Referral PT Treatment Report --> It can now handle multiple input files in batches or in sequence. --> Therefore, if the input files are for each month of the year, the add-on software/application would produce an output file for each of those months and properly store them in a folder. --> It is certainly doable to add a Graphical User Interface (GUI) to empower people to produce reports by a click of a button. --> I've observed similar software running on the computers/machines of local banks already being used to partly handle deposits and queuing. --> It would be good if such software/machines can also conveniently handle cash payments, similar to those in convenient stores and post offices abroad.

--- Master List/generateDoctorReferralPTTreatmentReportFromMasterList/java/generateDoctorReferralPTTreatmentReportFromMasterList.java
+++ Master List/generateDoctorReferralPTTreatmentReportFromMasterList/java/generateDoctorReferralPTTreatmentReportFromMasterList.java
@@ -21,7 +21,6 @@ import java.util.regex.Matcher;
 import java.text.NumberFormat;
 
 /*
-
 ' Given:
 ' 1) Encoding for the Month Input Worksheet
 ' --> Saved/Exported as "Tab delimited" .txt file from Excel
@@ -30,11 +29,23 @@ import java.text.NumberFormat;
 ' Output:
 ' 1) Auto-generated Medical Doctor Referral PT Treatment Report
 ' --> "Tab delimited" .txt file 
+' --> If the name of the input file is "input201808.txt", the output file will be "input201808Output.txt".
+'
+' Notes:
+' 1) To execute the add-on software/application simply use the following command:
+'   java generateDoctorReferralPTTreatmentReportFromMasterList input201801.txt
+' 
+' where: "input201801.txt" is the name of the file.
+' 
+' 2) To execute a set of input files, e.g. input201801.txt, input201802.txt, you can use the following command: 
+'  java generateDoctorReferralPTTreatmentReportFromMasterList input*
+'
+' where: "input*" means any file in the directory that starts with "input".
 */ 
 
 public class generateDoctorReferralPTTreatmentReportFromMasterList {	
 	private static boolean inDebugMode = false;
-	private static String inputFilename = "input201808"; //without extension
+	private static String inputFilename = "input201801"; //without extension; default input file
 	
 	private static String date = null;
 	
@@ -60,149 +71,163 @@ public class generateDoctorReferralPTTreatmentReportFromMasterList {
 	private static final int OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN = 5;
 	private static final int OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN = 6;
 	private static final int OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN = 7;
-		
-		
+				
 	public static void main ( String[] args ) throws Exception
 	{
-		PrintWriter writer = new PrintWriter(inputFilename+"Output.txt", "UTF-8");
+		//edited by Mike, 20181030
+		for (int i=0; i<args.length; i++) {
+			//added by Mike, 20181030
+			inputFilename = args[i].replaceAll(".txt","");			
+			makeFilePath("output"); //"output" is the folder where I've instructed the add-on software/application to store the output file
+			
+			PrintWriter writer = new PrintWriter("output/"+inputFilename+"Output.txt", "UTF-8");			
+			File f = new File(inputFilename+".txt");
+
+			Scanner sc = new Scanner(new FileInputStream(f));				
+
+			referringDoctorContainer = new HashMap<String, double[]>();
 		
-		File f = new File(inputFilename+".txt");
-
-		Scanner sc = new Scanner(new FileInputStream(f));				
-
-		referringDoctorContainer = new HashMap<String, double[]>();
-	
-		String s;		
-		s=sc.nextLine(); //skip the first row, which is the input file's table headers
-
-		//count/compute the number-based values of inputColumns 
-		while (sc.hasNextLine()) {
-			s=sc.nextLine();
+			String s;		
+			s=sc.nextLine(); //skip the first row, which is the input file's table headers
 			
-			//if the row is blank
-			if (s.trim().equals("")) {
-				continue;
-			}
-			
-			String[] inputColumns = s.split("\t");					
-			
-			if (date==null) {
-				date = getMonthYear(inputColumns[INPUT_DATE_COLUMN]);
-			}
-
-			if (!referringDoctorContainer.containsKey(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])) {
-				columnValuesArray = new double[OUTPUT_TOTAL_COLUMNS];
+			//count/compute the number-based values of inputColumns 
+			while (sc.hasNextLine()) {
+				s=sc.nextLine();
 				
-				if (inputColumns[INPUT_CLASS_COLUMN].contains("HMO")) {
-					columnValuesArray[OUTPUT_HMO_COUNT_COLUMN] = 1;
-					columnValuesArray[OUTPUT_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+				//if the row is blank
+				if (s.trim().equals("")) {
+					continue;
+				}
+				
+				String[] inputColumns = s.split("\t");					
 
-					if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
-						columnValuesArray[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+				if (date==null) {
+					date = getMonthYear(inputColumns[INPUT_DATE_COLUMN]);
+				}
+
+				if (!referringDoctorContainer.containsKey(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])) {
+					columnValuesArray = new double[OUTPUT_TOTAL_COLUMNS];
+					
+					if (inputColumns[INPUT_CLASS_COLUMN].contains("HMO")) {
+						columnValuesArray[OUTPUT_HMO_COUNT_COLUMN] = 1;
+						columnValuesArray[OUTPUT_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+
+						if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
+							columnValuesArray[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
+						else {
+							columnValuesArray[OUTPUT_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
 					}
 					else {
-						columnValuesArray[OUTPUT_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						columnValuesArray[OUTPUT_NON_HMO_COUNT_COLUMN] = 1;
+						columnValuesArray[OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+
+						if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
+							columnValuesArray[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
+						else {
+							columnValuesArray[OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
 					}
+
+					referringDoctorContainer.put(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN], columnValuesArray);
 				}
 				else {
-					columnValuesArray[OUTPUT_NON_HMO_COUNT_COLUMN] = 1;
-					columnValuesArray[OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-
-					if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
-						columnValuesArray[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+					if (inputColumns[INPUT_CLASS_COLUMN].contains("HMO")) {
+						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_COUNT_COLUMN]++;					
+						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] 
+							+= Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+							
+						if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
+							referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
+						else {
+							referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
 					}
 					else {
-						columnValuesArray[OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] = Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_COUNT_COLUMN]++;					
+						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] 
+							+= Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+							
+						if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
+							referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
+						else {
+							referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
+						}
 					}
 				}
+			}			
+			
+			/*
+			 * --------------------------------------------------------------------
+			 * OUTPUT
+			 * --------------------------------------------------------------------
+			*/
+			writer.print("HMO Report\n");
+			 
+			//init table header names
+			writer.print("DATE:\t"); //"DATE:" column
+			writer.print("REFERRING DOCTOR:\t"); //"REFERRING DOCTOR:" column
+			writer.print("COUNT:\t"); //"COUNT:" column
+			writer.print("TOTAL NET TREATMENT FEE:\t"); //"TOTAL NET TREATMENT FEE:" column
+			writer.print("PAID NET TREATMENT FEE:\t"); //"PAID NET TREATMENT FEE:" column
+			writer.print("UNPAID NET TREATMENT FEE:\t"); //"UNPAID NET TREATMENT FEE:" column		
+			writer.println("5% SHARE OF NET PAID:"); //"5% SHARE OF NET PAID:" column		
 
-				referringDoctorContainer.put(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN], columnValuesArray);
+			SortedSet<String> sortedKeyset = new TreeSet<String>(referringDoctorContainer.keySet());
+			
+			for (String key : sortedKeyset) {
+				writer.println( date + 
+								"\t" + key +
+								"\t" + (int) referringDoctorContainer.get(key)[OUTPUT_HMO_COUNT_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN]*0.05
+								); 				   							
 			}
-			else {
-				if (inputColumns[INPUT_CLASS_COLUMN].contains("HMO")) {
-					referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_COUNT_COLUMN]++;					
-					referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] 
-						+= Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-						
-					if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
-						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-					}
-					else {
-						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-					}
-				}
-				else {
-					referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_COUNT_COLUMN]++;					
-					referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] 
-						+= Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-						
-					if (inputColumns[INPUT_NOTES_COLUMN].contains("paid:")) {
-						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-					}
-					else {
-						referringDoctorContainer.get(inputColumns[INPUT_REFERRING_DOCTOR_COLUMN])[OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] += Double.parseDouble(inputColumns[INPUT_NET_PF_COLUMN]);
-					}
-				}
+			
+			writer.print("\nNON-HMO Report\n");
+			
+			//init table header names
+			writer.print("DATE:\t"); //"DATE:" column
+			writer.print("REFERRING DOCTOR:\t"); //"REFERRING DOCTOR:" column
+			writer.print("COUNT:\t"); //"COUNT:" column
+			writer.print("TOTAL NET TREATMENT FEE:\t"); //"TOTAL NET TREATMENT FEE:" column
+			writer.print("PAID NET TREATMENT FEE:\t"); //"PAID NET TREATMENT FEE:" column
+			writer.print("UNPAID NET TREATMENT FEE:\t"); //"UNPAID NET TREATMENT FEE:" column
+			writer.println("5% SHARE OF NET PAID:"); //"5% SHARE OF NET PAID:" column		
+					
+			for (String key : sortedKeyset) {
+				writer.println( date + 
+								"\t" + key +
+								"\t" + (int) referringDoctorContainer.get(key)[OUTPUT_NON_HMO_COUNT_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] +
+								"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN]*0.05
+								); 				   							
 			}
-		}			
-		
-		/*
-		 * --------------------------------------------------------------------
-		 * OUTPUT
-		 * --------------------------------------------------------------------
-		*/
-		writer.print("HMO Report\n");
-		
-		//init table header names
-		writer.print("DATE:\t"); //"DATE:" column
-		writer.print("REFERRING DOCTOR:\t"); //"REFERRING DOCTOR:" column
-		writer.print("COUNT:\t"); //"COUNT:" column
-		writer.print("TOTAL NET TREATMENT FEE:\t"); //"TOTAL NET TREATMENT FEE:" column
-		writer.print("PAID NET TREATMENT FEE:\t"); //"PAID NET TREATMENT FEE:" column
-		writer.print("UNPAID NET TREATMENT FEE:\t"); //"UNPAID NET TREATMENT FEE:" column		
-		writer.println("5% SHARE OF NET PAID:"); //"5% SHARE OF NET PAID:" column		
-
-		SortedSet<String> sortedKeyset = new TreeSet<String>(referringDoctorContainer.keySet());
-		
-		for (String key : sortedKeyset) {
-			writer.println( date + 
-							"\t" + key +
-							"\t" + (int) referringDoctorContainer.get(key)[OUTPUT_HMO_COUNT_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_HMO_PAID_NET_TREATMENT_FEE_COLUMN]*0.05
-							); 				   							
+			
+			sc.close();
+			writer.close();
 		}
-		
-		writer.print("\nNON-HMO Report\n");
-		
-		//init table header names
-		writer.print("DATE:\t"); //"DATE:" column
-		writer.print("REFERRING DOCTOR:\t"); //"REFERRING DOCTOR:" column
-		writer.print("COUNT:\t"); //"COUNT:" column
-		writer.print("TOTAL NET TREATMENT FEE:\t"); //"TOTAL NET TREATMENT FEE:" column
-		writer.print("PAID NET TREATMENT FEE:\t"); //"PAID NET TREATMENT FEE:" column
-		writer.print("UNPAID NET TREATMENT FEE:\t"); //"UNPAID NET TREATMENT FEE:" column
-		writer.println("5% SHARE OF NET PAID:"); //"5% SHARE OF NET PAID:" column		
-				
-		for (String key : sortedKeyset) {
-			writer.println( date + 
-							"\t" + key +
-							"\t" + (int) referringDoctorContainer.get(key)[OUTPUT_NON_HMO_COUNT_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_TOTAL_NET_TREATMENT_FEE_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_UNPAID_NET_TREATMENT_FEE_COLUMN] +
-							"\t" + referringDoctorContainer.get(key)[OUTPUT_NON_HMO_PAID_NET_TREATMENT_FEE_COLUMN]*0.05
-							); 				   							
-		}
-		
-		sc.close();
-		writer.close();
 	}
 	
 	private static String getMonthYear(String date) {
 		StringBuffer sb = new StringBuffer(date);				
 		return sb.substring(0,3).concat("-").concat(sb.substring(sb.length()-2,sb.length()));
+	}
+	
+	//added by Mike, 20181030
+	private static void makeFilePath(String filePath) {
+		File directory = new File(filePath);		
+		if (!directory.exists() && !directory.mkdirs()) 
+    	{
+    		System.out.println("File Path to file could not be made.");
+    	}    			
 	}
 }

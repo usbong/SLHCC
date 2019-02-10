c6272dc4501521f7db8d487949c446feeb157ccd
Michael B. Syson <masarapmabuhay@gmail.com>
2019-02-10 11:20:19 +0800

    +updated: VBA instructions to auto-compute and auto-generate the Monthly Summary Payment Report for both Consultation and PT Treatment transactions --> These reports include both HMO and NON-HMO/Cash payments. --> +updated: batch file --> work-in-progress

---
+++ Master List/generateMonthlySummaryPaymentReportOfAllInputFilesFromMasterList/Java Programming Language/Add-on Software Installer/generateMonthlySummaryReportOfPayments.bat
@@ -0,0 +1,3 @@
+java -cp .\software; generateMonthlyPTTreatmentSummaryReportOfAllInputFilesFromMasterList input/treatment/*.txt
+java -cp .\software; generateMonthlyConsultationSummaryReportOfAllInputFilesFromMasterList input/consultation/*.txt
+PAUSE
--- Master List/generateMonthlySummaryReportOfAllInputFilesFromMasterList/Java Programming Language/Add-on Software Installer/generateMonthlySummaryReportOfPayments.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportOfPaymentsOfAllInputFiles input/treatment/*.txt input/consultation/*.txt
-PAUSE

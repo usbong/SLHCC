1a03b786127dfd12751900368b9cdca7d384aaab
Michael B. Syson <masarapmabuhay@gmail.com>
2019-03-13 12:37:43 +0800

    +updated: files in the directory --> I understand that I will have to make time to properly organize these files. --> Interestingly, it seems that the way to automatically organize these to my delight is to remove the variations. --> By this I mean that the computer automatically consolidates or removes files such that there are only a few, perhaps only three (3), remaining files that I can choose to use to do what must be done. --> I understand, however, that variety is the natural result when a person moves out of his/herself, meets people, and learns that they have their own needs and wants given their contexts. --> Yet as we've learned in quality management, productivity diminishes with increasing variations, i.e. deviations. --> work-in-progress

--- generateMonthlySummaryReportForHMOBilling/generateMonthlySummaryReportForHMOBilling.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportForHMOBillingOfAllInputFiles input/treatment/*.txt input/consultation/*.txt
-PAUSE
--- generateMonthlySummaryReportForHMOBilling/generateMonthlyUtilizationSummaryReportForHMOBilling.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlyUtilizationSummaryReportForHMOBillingOfAllInputFiles input/treatment/*.txt input/consultation/*.txt
-PAUSE

70b69546b6ba4de3e65c8ff6a51aa1a5862f38b4
Michael B. Syson <masarapmabuhay@gmail.com>
2019-04-01 15:07:30 +0800

    +added: add-on software installer folder in the history folder +added: MS EXCEL workbooks with the HMO Billing add-on software/Macro to automatically generate the HMO Billing Utilization and Summary reports --> This update should eliminate excess files, folders, and action steps. --> I also note that this update is feedback/advice from our partner department/unit member. --> From computer history, we learn that if the cost of human labor remains cheaper than that of software/computer/machines, the people must be mechanical and devoid of emotions for a group to be continuously and substantially more productive. --> However, given that humans do have emotions, it appears to me that to be productive the number of humans active in a department/unit must not exceed four (4). --> In addition, a member from another department/unit can occasionally offer technical, albeit temporary assistance. --> Other forms of assistance must come from software/computer/machines. --> Indeed, nations and even regions within nations that lagged behind in mechanization tend to remain poor and exploited by those that advanced faster. --> work-in-progress

---
+++ SLHCC/Billing/generateMonthlySummaryReportForHMOBilling/history/add-on software installer/generateMonthlySummaryReportForHMOBillingNonMasterList.bat
@@ -0,0 +1,2 @@
+java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportForHMOBillingOfAllInputFilesNonMasterList input/treatment/*.txt input/consultation/*.txt
+PAUSE

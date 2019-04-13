fb3d5f0f59427648526526a74cbecfa4461093ed
Michael B. Syson <masarapmabuhay@gmail.com>
2019-04-13 09:49:04 +0800

    +verified: the output data from the component parts for computing the diagnosed cases --> The output files from the combined sets of instructions are already equal with those in the previous separate sets of instructions. --> TO-DO: -add: process diagnosed cases with commas as multiple cases, instead of only one case per transaction --> TO-DO: -remove excess system prints in the CMD prompt --> TO-DO: -remove excess portions in the add-on software --> TO-DO: -update: the instructions to make the virtual writers write only one (1) output file +deleted: previously used batch files and software files --> These files contain sets of instructions. +updated: Macro/set of instructions in MS EXCEL workbook to use the updated batch file to automatically generate the Monthly Summary Report with the Diagnosed Cases

--- generateMonthlySummaryReport/add-on software/generateMonthlySummaryReport.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportOfAllInputFiles input/treatment/*.txt input/consultation/*.txt
-PAUSE
--- generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportOfDiagnosedCases.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportOfDiagnosedCasesOfAllInputFiles input/treatment/*.txt input/consultation/*.txt assets/*.txt
-PAUSE

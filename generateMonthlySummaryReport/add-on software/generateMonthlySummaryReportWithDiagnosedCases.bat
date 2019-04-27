e8a074957a74c57b9697623be9be58070bb364eb
Michael B. Syson <masarapmabuhay@gmail.com>
2019-04-28 07:53:59 +0800

    +updated: instructions to resolve the issue when the directory where the workbook and the add-on software are located is in another storage drive, e.g. Drive G:, instead of Drive C: --> I note that this storage drive can be a USB Flash Drive, another partition of the same Hard Disk Drive, etc. --> work-in-progress

--- generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportWithDiagnosedCases.bat
+++ generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportWithDiagnosedCases.bat
@@ -1,3 +1,4 @@
+cd /d %1
 java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportWithDiagnosedCasesOfAllInputFiles input/treatment/*.txt input/consultation/*.txt assets/*.txt
 cd ..
 "add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///%CD%/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///%CD%/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///%CD%/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html

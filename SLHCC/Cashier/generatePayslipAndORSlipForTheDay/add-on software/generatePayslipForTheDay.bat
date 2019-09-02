7c905d64b8720608a132b7bbc009eb67c3803891
Michael B. Syson <masarapmabuhay@gmail.com>
2019-09-02 20:32:50 +0800

    +updated: batch file, i.e. set of instructions, for both the computers at the Cashier Window and the ORTHO & PT Unit to automatically close the Command Prompt Window after executing the instructions --> With this, both the Cashier person and the Unit member do not need to press any key to close the Command Prompt Window. +updated: batch file at the ORTHO & PT Unit to set the main directory to where the batch file is located --> This is instead of setting the main directory to where the set of instructions that executed the batch file is located. --> I note that I wrote that set of instructions using the PHP (PHP: Hypertext Preprocessor) language. --> In addition, at present, the computer server is also a computer client that gets the transactions list from the database (DB). --> work-in-progress

--- SLHCC/Cashier/generatePayslipAndORSlipForTheDay/add-on software/generatePayslipForTheDay.bat
+++ SLHCC/Cashier/generatePayslipAndORSlipForTheDay/add-on software/generatePayslipForTheDay.bat
@@ -1,4 +1,5 @@
 set mainDirectory=%CD%
 cd /d %mainDirectory%
 java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect input/*.txt
-PAUSE
+#PAUSE
+exit

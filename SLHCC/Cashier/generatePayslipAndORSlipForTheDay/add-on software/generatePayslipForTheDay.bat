fa54d17c72741c6e38fae1a6ff7e1f04f4429dfd
Michael B. Syson <masarapmabuhay@gmail.com>
2019-09-14 16:01:20 +0800

    +updated: sets of instructions for the computer to automatically open the MS EXCEL workbook with the Payslip worksheet, etc --> I note that the computer automatically closes the MS EXCEL workbook after exporting, i.e. saving, the Export worksheet with the Payslip details as a Tab-delimited .txt file. --> At present, from my understanding, if I don't instruct the computer to close the MS EXCEL workbook, the computer makes and renames the MS EXCEL workbook as the Exported worksheet. --> From experience, the Unit member and/or Cashier person will likely begin to update the incorrect MS EXCEL workbook. --> work-in-progress

--- SLHCC/Cashier/generatePayslipAndORSlipForTheDay/add-on software/generatePayslipForTheDay.bat
+++ SLHCC/Cashier/generatePayslipAndORSlipForTheDay/add-on software/generatePayslipForTheDay.bat
@@ -1,5 +1,7 @@
 set mainDirectory=%~dp0
 cd /d %mainDirectory%
 java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect input/*.txt
+cd ..
+%2
 #PAUSE
 exit

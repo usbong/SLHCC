d471ab1831bfc37e8b3f3a5c998f7b11b0219dcf
Michael B. Syson <masarapmabuhay@gmail.com>
2019-09-14 11:59:14 +0800

    +updated: sets of instructions for the computer to automatically execute the add-on software instructions to send a request to the computer server to store the Payslip details in the database (DB) repository. --> work-in-progress

--- SLHCC/Cashier/generatePayslipAndORSlipForTheDay/add-on software/generatePayslipForTheDay.bat
+++ SLHCC/Cashier/generatePayslipAndORSlipForTheDay/add-on software/generatePayslipForTheDay.bat
@@ -1,4 +1,4 @@
-set mainDirectory=%CD%
+set mainDirectory=%~dp0
 cd /d %mainDirectory%
 java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect input/*.txt
 #PAUSE

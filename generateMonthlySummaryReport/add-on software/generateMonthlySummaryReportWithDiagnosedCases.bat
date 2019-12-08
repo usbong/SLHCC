4f70d451e3b40d8bc64b7097791967bf467ea11d
Masarap Mabuhay <masarapmabuhay@gmail.com>
2019-12-08 08:58:07 +0800

    +updated: instructions for the computer to automatically make a temporary folder and put inside the current transactions lists
    --> This is before the computer processes the transactions using another set of instructions that I wrote in the Java programming language.
    --> This resolves the issue pertaining to the lists remaining deleted when the computer encounters an error.
    --> I note that I got confused with the language syntax of my Command Prompt instructions, e.g. whether to use back slash or forward slash, to put quotation marks or not.
    --> work-in-progress

--- generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportWithDiagnosedCases.bat
+++ generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportWithDiagnosedCases.bat
@@ -1,12 +1,22 @@
+cd /d %1%
 set mainDirectory=%CD%
-cd assets/transactions/
-copy *List.txt *ListTemp.txt
 cd /d %mainDirectory%
+cd assets/transactions/
+mkdir tempListBeforeProcessing
+copy *List.txt "tempListBeforeProcessing/"
+
+xcopy "tempListBeforeProcessing\*List.txt" "." /s
+
+cd %mainDirectory%
+		
 java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportWithDiagnosedCasesOfAllInputFiles input/treatment/*.txt input/consultation/*.txt assets/*.txt
+
 cd assets/transactions/
 del *List.txt
+
 rename *ListTemp.txt *List.txt
+
 cd %mainDirectory%
 cd ..
-"add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///%CD%/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///%CD%/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///%CD%/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///%CD%/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///%CD%/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///%CD%/add-on%%20software/output/MonthlyStatisticsTreatment.html
+"add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
 PAUSE

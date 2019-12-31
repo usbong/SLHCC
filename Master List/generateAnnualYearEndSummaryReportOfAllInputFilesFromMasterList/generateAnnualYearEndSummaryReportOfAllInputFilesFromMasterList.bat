0c35911aaef8bf21b75714090133168ba7c65ef0
Masarap Mabuhay <masarapmabuhay@gmail.com>
2019-12-31 11:24:52 +0800

    Add files via upload
    +updated: batch file, i.e. set of instructions, for the computer to use as input, files inside both the "consultation" and "treatment" folders, which are located inside the "input" folder
    --> Previously, the Unit member put the input files for both Consultation and PT Treatment all inside "input" folder.
    --> This action makes storage of information for a human person organized.
    --> work-in-progress

--- Master List/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList.bat
+++ Master List/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList.bat
@@ -1,2 +1,2 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList input/*.txt assets/*.txt
+java -cp .\software;.\software\org.apache.commons.text.jar generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList input/consultation/*.txt input/treatment/*.txt assets/*.txt
 PAUSE

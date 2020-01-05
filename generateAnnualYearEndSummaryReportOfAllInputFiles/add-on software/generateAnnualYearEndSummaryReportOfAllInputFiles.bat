649fb9da1ccda0b3a57e434916820cf880cdf722
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-01-05 17:59:07 +0800

    Add files via upload
    +updated: instructions for the computer to automatically put the input files inside the correct folder, i.e. Consultation or PT Treatment.
    --> work-in-progress

--- generateAnnualYearEndSummaryReportOfAllInputFiles/add-on software/generateAnnualYearEndSummaryReportOfAllInputFiles.bat
+++ generateAnnualYearEndSummaryReportOfAllInputFiles/add-on software/generateAnnualYearEndSummaryReportOfAllInputFiles.bat
@@ -1,5 +1,15 @@
 cd /d %1%
 set mainDirectory=%CD%
 cd /d %mainDirectory%
+
+cd..
+
+copy "C:\Usbong\unit\workbooks\Consultation*.txt" "add-on software\input\consultation"
+
+copy "C:\Usbong\unit\workbooks\Treatment*.txt" "add-on software\input\treatment"
+
+cd "add-on software"
+
 java -cp .\software;.\software\org.apache.commons.text.jar generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList input/consultation/*%2*.txt input/treatment/*%2*.txt assets/*.txt
+
 PAUSE

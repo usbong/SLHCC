70d812bd5f501dd7dac7e8a6c9906139e83c954b
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-09-17 11:02:31 +0800

    Add files via upload

--- generateReportOfPaymentsForTheDay/add-on software/generatePayslipForTheDayReportToSendViaSMS.sh
+++ generateReportOfPaymentsForTheDay/add-on software/generatePayslipForTheDayReportToSendViaSMS.sh
@@ -10,8 +10,17 @@ cd "$mainDirectory"
 # Java command using Windows Machine
 # java -cp .\software:.\software\org.json.jar:.\software\org.apache.httpclient.jar:.\software\org.apache.httpcore.jar:.\software\org.apache.commons-logging.jar UsbongSMSReportMain http://localhost/ halimbawa
 
+# example output: 2020-09-17
+# echo $(date +%F)
+
+# example output: September 2020
+# echo $(date +%B) $(date +%Y)
+sMonthYear=$(date +%B)" "$(date +%Y)
+
+# echo $sMonthYear
+
 # Java command using Linux Machine
-java -cp ./software:./software/org.json.jar:./software/org.apache.httpclient.jar:./software/org.apache.httpcore.jar:./software/org.apache.commons-logging.jar UsbongSMSReportMain http://localhost/ ./input/*.txt
+java -cp ./software:./software/org.json.jar:./software/org.apache.httpclient.jar:./software/org.apache.httpcore.jar:./software/org.apache.commons-logging.jar UsbongSMSReportMain http://localhost/ ./input/*"$sMonthYear".txt
 
 # java -cp .\software:.\software\org.json.jar:.\software\org.apache.httpclient.jar:.\software\org.apache.httpcore.jar:.\software\org.apache.commons-logging.jar ./software/UsbongReportMain http://localhost/ halimbawa
 

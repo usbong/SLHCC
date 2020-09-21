7428354e277315b5153e9d6189da9930236ebdca
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-09-21 18:16:49 +0800

    Add files via upload

--- generateReportOfPaymentsForTheDay/add-on software/sendSMSCommandFromWindowsPC.bat
+++ generateReportOfPaymentsForTheDay/add-on software/sendSMSCommandFromWindowsPC.bat
@@ -30,41 +30,9 @@ REM last accessed: 20200914; answer by: LionCoder, 20111213T0256; edited by Comm
 
 REM added by Mike, 20200921
 rem cmd /c "generatePayslipForTheDayReportToSendViaSMS.bat"
-rem Start-Process -FilePath "cmd" -ArgumentList "/c generatePayslipForTheDayReportToSendViaSMS"
+rem Start-Process -FilePath "cmd" -ArgumentList "/c 
 
-rem TO-DO: -update: this
-@echo off
-set mainDirectory=%~dp0
-cd /d %mainDirectory%
-cd output/
-del *.txt
-cd ..
-
-rem added by Mike, 20200921
-rem set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
-rem set myTime=%time:~0,2%%time:~3,2%
-
-set myYear=%date:~10,4%
-
-echo %myYear%
-
-rem reference: https://stackoverflow.com/questions/15469307/how-to-print-month-name-in-file-name-by-using-bat/48331435
-rem answer by: Aacini, 20130318, edited 20170724
-setlocal EnableDelayedExpansion
-set m=100
-for %%m in (January February March April May June July August September October November  December) do (
-   set /A m+=1
-   set month[!m:~-2!]=%%m
-)
-rem Change tokens=2 for DD/MM/YYYY date format
-rem edited by Mike, 20200921
-rem for /F "tokens=1 delims=/"  %%m in ("%date:%") do (
-for /F "tokens=1 delims=/"  %%m in ("%date:~4,2%") do (
-   set monthName=!month[%%m]!
-)
-echo %monthName%
-
-java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongSMSReportMain http://localhost/ input/""Treatment*%monthName%" "%myYear%.txt""
+call generatePayslipForTheDayReportToSendViaSMS.bat
 
 REM D:\Usbong\SLHCC\workbooks
 

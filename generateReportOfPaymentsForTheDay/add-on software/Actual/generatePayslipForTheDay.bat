baea6a0e1305e321103d3b56693b079f2c92efec
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-09-27 09:12:20 +0800

    Delete generatePayslipForTheDay.bat

--- generateReportOfPaymentsForTheDay/add-on software/Actual/generatePayslipForTheDay.bat
+++
@@ -1,8 +0,0 @@
-set mainDirectory=%~dp0
-cd /d %mainDirectory%
-cd output/
-del *.txt
-cd ..
-java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect http://localhost/ input/*.txt
-#PAUSE
-exit

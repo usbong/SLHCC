4b2b0ef2fdf2cb06e2629fc6f14aada06fb880ba
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-12-27 12:21:43 +0800

    Add files via upload

--- Master List/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList.bat
+++ Master List/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList/generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList.bat
@@ -1,2 +1,6 @@
-java -cp .\software;.\software\org.apache.commons.text.jar generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList input/consultation/*.txt input/treatment/*.txt assets/*.txt
+rem edited by Mike, 20201227
+rem java -cp .\software;.\software\org.apache.commons.text.jar generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList input/consultation/*.txt input/treatment/*.txt assets/*.txt
+
+java -cp .\software;.\software\org.apache.commons.text.jar generateAnnualYearEndSummaryReportOfAllInputFilesFromMasterList input/consultation/*.csv input/treatment/*.csv assets/*.txt
+
 PAUSE

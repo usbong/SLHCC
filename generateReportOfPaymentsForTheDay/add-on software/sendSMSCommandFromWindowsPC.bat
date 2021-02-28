70b1edf52993ff4bed84ec788537ee36ad920c7e
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-02-28 18:14:48 +0800

    Add files via upload

--- generateReportOfPaymentsForTheDay/add-on software/sendSMSCommandFromWindowsPC.bat
+++ generateReportOfPaymentsForTheDay/add-on software/sendSMSCommandFromWindowsPC.bat
@@ -1,8 +1,19 @@
 @echo off
+REM
+REM Copyright 2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM 
+REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+REM
 REM Send Short Messaging Service (SMS) message from Windows Personal Computer (PC)
-REM author: SYSON, MICHAEL B.
-REM date created: 20200915
-REM last updated: 20210228
+REM
+REM @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM @author: SYSON, MICHAEL B.
+REM @date created: 20200915
+REM @last updated: 20210228
 REM
 REM Notes:
 REM 1) Download Android Software Development Kit (SDK) Platform Tools to execute Android Debug Bridge (ADB) Shell commands

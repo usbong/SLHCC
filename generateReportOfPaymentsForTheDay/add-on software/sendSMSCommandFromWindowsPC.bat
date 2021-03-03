9495be55fe56fc638eb2d37262da9ccaed063fd3
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-03-03 18:02:40 +0800

    Add files via upload

--- generateReportOfPaymentsForTheDay/add-on software/sendSMSCommandFromWindowsPC.bat
+++ generateReportOfPaymentsForTheDay/add-on software/sendSMSCommandFromWindowsPC.bat
@@ -1,5 +1,4 @@
 @echo off
-REM
 REM Copyright 2020~2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
 REM 
 REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
@@ -13,7 +12,7 @@ REM
 REM @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
 REM @author: SYSON, MICHAEL B.
 REM @date created: 20200915
-REM @last updated: 20210228
+REM @last updated: 20210303
 REM
 REM Notes:
 REM 1) Download Android Software Development Kit (SDK) Platform Tools to execute Android Debug Bridge (ADB) Shell commands
@@ -23,6 +22,7 @@ REM 2) Verify connected devices/emulators using ADB Command: adb shell
 REM 3) Verify ADB commands using: adb
 REM 4) Accept Rivest–Shamir–Adleman (RSA) pairing of mobile telephone device with PC
 REM  --> This is after connecting the device with the PC via the Universal Serial Bus (USB) ports and cable
+rem 5) Set value of "myDate" container if not report for today, e.g. set myDate=2021-03-03
 REM
 REM References:
 REM 1) https://stackoverflow.com/questions/17580199/sending-a-sms-on-android-through-adb;
@@ -30,74 +30,45 @@ REM last accessed: 20200914; question by: user790995, 20130710T2022
 REM 2) https://stackoverflow.com/questions/7789826/adb-shell-input-events;
 REM last accessed: 20200914; answer by: LionCoder, 20111213T0256; edited by Community, 20200317T0723
 
-REM added by Mike, 20200919; edited by Mike, 20200924
-Call generatePayslipForTheDayReportToSendViaSMS.bat
-
-REM TO-DO: -update: this
-REM echo $(date +%F)
-
-REM TO-DO: -update: this
-REM sSMSBodyValue=`cat "output/smsReport"$(date +%F)".txt"`
-rem set sSMSBodyValue=`type "output/smsReport"*".txt"`
-
-rem added by Mike, 20200924
-rem edited by Mike, 20201002
-rem set sDateToday=%date:~5%
-rem echo %sDateToday%
-
-rem set sMonthToday=0%date:~5,1%
-rem echo %sMonthToday%
-
-rem set sDayToday=%date:~7,2%
-rem echo %sDayToday%
-
-rem added by Mike, 20210228
-set sYearToday=%date:~10,4%
-echo %sYearToday%
-
-rem added by Mike, 20201002
+rem added by Mike, 20210303
 set myDate=%date:~10,4%-%date:~4,2%-%date:~7,2%
-rem set myDate="2020-10-01"
-rem set myDate="2021-01-04"
+rem note: no need to add quotation marks; put rem command in new line
+rem set myDate=2021-03-01
 echo %myDate%
 
-rem for %%i (type "D:\2020\add-on software\generatePayslipForTheDay\output\"smsReport2020-09-24.txt") do (set sSMSBodyValue=%%i)
-rem set sSMSBodyValue=for(type "D:\2020\add-on software\generatePayslipForTheDay\output\"smsReport2020-09-24.txt")
+rem added by Mike, 20210303
+rem set sYearToday=%date:~10,4%
+set sYearToday=%myDate:~0,4%
+echo %sYearToday%
 
-rem for (type "D:\2020\add-on software\generatePayslipForTheDay\output\"smsReport2020-09-24.txt") do (set sSMSBodyValue=%%i)
-rem print "D:\2020\add-on software\generatePayslipForTheDay\output\"smsReport2020-09-24.txt" | set sSMSBodyValue
+REM added by Mike, 20200919; edited by Mike, 20200924
+Call generatePayslipForTheDayReportToSendViaSMS.bat %myDate%
 
-rem set sSMSBodyValue="D:\2020\add-on software\generatePayslipForTheDay\output\"smsReport2020-09-24.txt"
-rem for /f "tokens=2" %%V in ('tasklist.exe ^| findstr /i "%1" 2^>NUL') do @set "PID_LIST=!PID_LIST! /PID %%V"
-rem for /f "delims=" %%V in ('type "D:\2020\add-on software\generatePayslipForTheDay\output\smsReport2020-09-24.txt"') do @set sSMSBodyValue=%%V
-rem for /f "delims=" %%V in ('type "D:\2020\add-on software\generatePayslipForTheDay\output\smsReport"%sYearToday%-%sMonthToday%-%sDayToday%".txt"') do @set sSMSBodyValue=%%V
+rem added by Mike, 20210302
+rem update folder location
+cd "D:\Usbong\SLHCC\Reports\platform-tools"
 
-rem edited by Mike, 20201002
-rem for /f "delims=" %%V in ('type "D:\2020\add-on software\generatePayslipForTheDay\output\smsReport"%sYearToday%-%sMonthToday%-%sDayToday%".txt"') do @set sSMSBodyValue=%%V
-rem for /f "delims=" %%V in ('type "D:\2020\add-on software\generatePayslipForTheDay\output\smsReport%myDate%.txt"') do @set sSMSBodyValue=%%V
+rem edited by Mike, 20210303
+rem update file location
+rem for /f "delims=" %%V in ('type "D:\%sYearToday%\add-on software\generatePayslipForTheDay\output\smsReport%myDate%.txt"') do @set sSMSBodyValue=%%V
 
-rem edited by Mike, 20210228
-rem for /f "delims=" %%V in ('type "D:\2021\add-on software\generatePayslipForTheDay\output\smsReport%myDate%.txt"') do @set sSMSBodyValue=%%V
-for /f "delims=" %%V in ('type "D:\%sYearToday%\add-on software\generatePayslipForTheDay\output\smsReport%myDate%.txt"') do @set sSMSBodyValue=%%V
+for /f "delims=" %%V in ('type "D:\Usbong\SLHCC\Reports\add-on software\output\smsReport%myDate%.txt"') do @set sSMSBodyValue=%%V
 
 echo %sSMSBodyValue%
 
+rem NOTES:
+rem -----
 rem set sSMSBodyValue=SLHCC,2021-01-04,PT,Total:8,CashTotalFee:1071.43,CashTotalNetFee:712.50,HMOTotalFee:3835.72,HMOTotalNetFee:2175.38
 
 REM update file location
-REM cd /home/unit_member/Documents/USBONG/Android/platform-tools
-rem cd "D:\2020\add-on software\sendReportViaSMS\platform-tools_r30.0.4-windows\platform-tools"
-rem edited by Mike, 20210116
-rem removed by Mike, 20210228
-rem set myDateYear=%date:~10,4%
 
-rem cd "D:\2020\add-on software\generatePayslipForTheDay\lib\platform-tools_r30.0.4-windows\platform-tools"
-rem edited by Mike, 20210228
-rem cd "D:\%myDateYear%\add-on software\generatePayslipForTheDay\lib\platform-tools_r30.0.4-windows\platform-tools"
-cd "D:\%sYearToday%\add-on software\generatePayslipForTheDay\lib\platform-tools_r30.0.4-windows\platform-tools"
+rem removed by Mike, 20210302
+rem cd "D:\%sYearToday%\add-on software\generatePayslipForTheDay\lib\platform-tools_r30.0.4-windows\platform-tools"
 
 rem adb shell am start -a android.intent.action.SENDTO -d sms:639299527263 --es sms_body "Kumusta!" --ez exit_on_sent true
 rem adb shell am start -a android.intent.action.SENDTO -d sms:639299527263 --es sms_body "output/smsReport"*".txt" --ez exit_on_sent true
+rem -----
+
 adb shell am start -a android.intent.action.SENDTO -d sms:639299527263 --es sms_body %sSMSBodyValue% --ez exit_on_sent true
 
 adb shell input keyevent 22

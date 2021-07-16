25e829dad44172766bf838f7618e84cef90cc970
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-07-16 12:44:27 +0800

    Add files via upload

--- Master List/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList.sh
+++ Master List/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-
+#
+# Copyright 2019~2021 SYSON, MICHAEL B.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
 # Auto-generate Unpaid HMO Summary Report of all input files from Master List using Linux Personal Computer (PC)
+#
+# company: USBONG
 # author: SYSON, MICHAEL B.
 # date created: 20201106
-# last updated: 20201109
+# last updated: 20210716
+# website address: http://www.usbong.ph
 #
 # Notes:
 # HMO = Health Maintenance Organization
@@ -12,5 +23,5 @@
 # where: 201912 : YYYYMM
 # note: add underscore, i.e. "_" before year and month
 
-java -cp ./software: generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList input/treatment/*.csv input/consultation/*.csv
+java -cp ./software: generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList input/treatment/*.csv input/consultation/*.csv assets/templates/generateUnpaidHMOSummaryReportOutputTemplate.html
 

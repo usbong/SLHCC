9dc55a0c79c38c14e9bf492bdd7f71c7e4b88d1a
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-10-18 19:41:27 +0800

    Add files via upload

--- Master List/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList.bat
+++ Master List/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList/generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList.bat
@@ -1,2 +1,34 @@
-java -cp .\software; generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList input/treatment/*.txt input/consultation/*.txt
+@ECHO OFF
+REM
+REM Copyright 2019~2021 SYSON, MICHAEL B.
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+REM
+REM Auto-generate Unpaid HMO Summary Report of all input files from Master List using Linux Personal Computer (PC)
+REM
+REM company: USBONG
+REM author: SYSON, MICHAEL B.
+REM date created: 20200217
+REM last updated: 20211018
+REM website address: http://www.usbong.ph
+REM
+REM Notes:
+REM HMO = Health Maintenance Organization
+REM
+REM Example input file: PT TREATMENT 2020verifiedMacroEnabledMasterListV61LibreOfficeCalc_201912
+REM where: 201912 : YYYYMM
+REM note: add underscore, i.e. "_" before year and month
+
+REM Linux Machine Command
+REM java -cp ./software: generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList input/treatment/*.csv input/consultation/*.csv assets/templates/generateUnpaidHMOSummaryReportOutputTemplate.html
+
+REM Windows Machine Command
+REM input .csv file format; update to use .txt file format if necessary
+REM java -cp .\software: generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList input/treatment/*.csv input/consultation/*.csv assets/templates/generateUnpaidHMOSummaryReportOutputTemplate.html
+java -cp .\software; generateUnpaidHMOSummaryReportOfAllInputFilesFromMasterList input/treatment/*.txt input/consultation/*.txt assets/templates/generateUnpaidHMOSummaryReportOutputTemplate.html
+
 PAUSE

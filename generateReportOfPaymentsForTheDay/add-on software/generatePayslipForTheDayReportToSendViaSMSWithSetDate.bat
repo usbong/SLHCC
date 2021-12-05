12b19a09732089ee559bff6edda49c3b947688b0
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-12-05 09:46:17 +0800

    Add files via upload

--- generateReportOfPaymentsForTheDay/add-on software/generatePayslipForTheDayReportToSendViaSMSWithSetDate.bat
+++ generateReportOfPaymentsForTheDay/add-on software/generatePayslipForTheDayReportToSendViaSMSWithSetDate.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Copyright 2020~2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM Copyright 2020~2021 SYSON, MICHAEL B.
 REM  
 REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 REM http://www.apache.org/licenses/LICENSE-2.0
@@ -7,10 +7,10 @@ REM
 REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
 REM
 REM Generate Payslip for the Day Report to Send via Short Messaging Service (SMS) message from Windows Personal Computer (PC)
-REM @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM @company: USBONG
 REM @author: SYSON, MICHAEL B.
 REM @date created: 2020
-REM @last updated: 20210614
+REM @last updated: 20211205; (from 20210703)
 REM @website address: http://www.usbong.ph
 
 REM TO-DO: -update: this
@@ -49,11 +49,20 @@ REM $sMonthYear=$(date +%B)" "$(date +%Y) REM Command using Linux Machine
 REM added by Mike, 20210612
 REM TO-DO: -add: auto-identify
 REM date format: M/d/yyyy
-set sSetDate=6/9/2021
-set sSetDateISOFormat=2021-06-09
+REM set sSetDate=6/14/2021
+
+REM added by Mike, 20211205
+REM reminder#1: with sSetDate, no additional zero for numbers <10
+REM reminder#2: with sSetDateISOFormat, add zero for numbers <10
+REM TO-DO: -add: auto-correct inputs
+set sSetDate=12/2/2021
+
+REM set sSetDateISOFormat=2021-06-14
+set sSetDateISOFormat=2021-12-02
 set myYear=2021
 rem add * to due to space between month and year
-set sMonthYear=June"*"2021
+REM set sMonthYear=June"*"2021
+set sMonthYear=December"*"2021
 
 REM Java command using Linux Machine
 REM java -cp ./software:./software/org.json.jar:./software/org.apache.httpclient.jar:./software/org.apache.httpcore.jar:./software/org.apache.commons-logging.jar UsbongSMSReportMain http://localhost/ ./input/*"$sMonthYear".txt
@@ -65,7 +74,7 @@ set sInputFileLocation="C:\Usbong\unit\workbooks"
 
 echo %sInputFileLocation%
 
-pause
+REM pause
 
 rem java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongSMSReportMain http://localhost/ "%sInputFileLocation%\"Treatment"*%sMonthYear%"2020.txt"
 REM edited by Mike, 20200926

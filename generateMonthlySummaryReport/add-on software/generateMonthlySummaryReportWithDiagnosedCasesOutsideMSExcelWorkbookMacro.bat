47df15cfeffad29ea7c2f5173e054da02e12f3b7
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-07-31 13:07:57 +0800

    Add files via upload

--- generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportWithDiagnosedCasesOutsideMSExcelWorkbookMacro.bat
+++ generateMonthlySummaryReport/add-on software/generateMonthlySummaryReportWithDiagnosedCasesOutsideMSExcelWorkbookMacro.bat
@@ -1,4 +1,4 @@
-@echo OFF
+
 REM ' Copyright 2019~2022 SYSON, MICHAEL B.
 REM '
 REM ' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
@@ -12,15 +12,14 @@ REM ' @author: SYSON, MICHAEL B.
 REM ' @date created: 2019
 REM ' @date updated: 20220731; from 20220414
 REM ' @website address: http://www.usbong.ph
+REM '
+REM ' Additional Note:
+REM ' 1) This batch file executes outside of the MS Excel Workbook Macro 
 
 cd /d %1%
 set mainDirectory=%CD%
 cd /d %mainDirectory%
-
-REM 'edited by Mike, 20220731
-REM cd assets/transactions/
-cd "add-on software/assets/transactions/"
-
+cd assets/transactions/
 mkdir tempListBeforeProcessing
 copy *List.txt "tempListBeforeProcessing/"
 
@@ -30,17 +29,9 @@ cd %mainDirectory%
 	
 REM edited by Mike, 20220414	
 REM java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportWithDiagnosedCasesOfAllInputFiles input/treatment/*.txt input/consultation/*.txt assets/*.txt
-
-REM 'added by Mike, 20220731
-cd "add-on software/"
-
 java -cp .\software;.\software\org.apache.commons.text.jar generateMonthlySummaryReportWithDiagnosedCasesOfAllInputFiles input/treatment/*.txt input/consultation/*.txt assets/KnownDiagnosedCasesList.txt
 
-REM 'edited by Mike, 20220731
 cd assets/transactions/
-REM cd "add-on software/assets/transactions/"
-
-
 del *List.txt
 
 rename *ListTemp.txt *List.txt
@@ -51,10 +42,6 @@ rem edited by Mike, 20210131
 rem "add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/2020/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/2020/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/2020/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/2020/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/2020/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/2020/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
 set myDateYear=%date:~10,4%
 
-REM 'edited by Mike, 20220731
-REM "add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
-
-"generateMonthlySummaryReport\add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
-
+"add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/%myDateYear%/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
 
 PAUSE
